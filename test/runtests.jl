71c3f84186b49764abf7f6d656bc504ff0c6d7d5
Jordan Jalving <jhjalving@gmail.com>
2019-03-27 17:38:33 -0500

    add files

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
-using ComputingGraphs
 using Test
 
-@testset "ComputingGraphs.jl" begin
-    # Write your own tests here.
-end
+println("Testing State Manager Functions")
+@test include("test_state_manager.jl")
+
+println("Testing Computing Graph Functions")
+
+println("Testing Simple Computing Graph")
+@test include("test_simple_node.jl")
+
+println("Testing Continuous Node")
+@test include("test_continuous_node.jl")
